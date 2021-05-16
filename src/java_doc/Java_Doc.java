91b6f913d311556ac48fc6961883c2a88e3fb817
ROCIO <ROCIO@HERNANDEZ>
2021-05-15 22:03:11 -0500

    EjercicioDocss

---
+++ src/java_doc/Java_Doc.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_doc;
+
+/**
+ *
+ * @author ROCIO
+ */
+public class Java_Doc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                int n = 0;
+        for (int i=1;i<=4;i++){
+        n=n+i;
+        }
+        System.out.println("El factorial de 4 es "+ n);
+    
+
+    }
+    
+}
